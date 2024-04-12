c31cf5296e1a8f5eb007c0da272262145b0cc68b
kim-moai <xogks12345@naver.com>
2024-04-12 16:03:34 +0900

    ☠️회원가입-로그인 로직 구현(오류)
    회원가입-로그인 로직 구현 하던 중
    오류 발생, 해결 못함

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
-from fastapi import FastAPI, UploadFile,Form,Response
+from fastapi import FastAPI, UploadFile, Form, Response, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
 con = sqlite3.connect('db.db', check_same_thread=False)
 cur = con.cursor()
 
-cur.execute(f"""
-            CREATE TABLE IF NOT EXISTS items (
-	             id INTEGER PRIMARY KEY,
-	             title TEXT NOT NULL,
-	             image BLOB,
-	             price INTEGER NOT NULL,
-	             description TEXT,
-	             place TEXT NOT NULL,
-	             insertAt INTEGER NOT NULL
-            );
-            """)
-
 app = FastAPI()
+
+SECRET = "super-coding"
+manager = LoginManager(SECRET, '/login')
+
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'''id="{data}"'''
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'''id="{data[id]}"'''
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(f"""
+                       SELECT * from users WHERE {WHERE_STATEMENTS}
+                       """).fetchone()
+    return user     
+
+@app.post('/login')
+def login(id:Annotated[str,Form()],
+           password:Annotated[str,Form()]):
+    user = query_user(id)
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+    
+    access_token = manager.create_access_token(data={
+        'sub' : {
+            'id' : user['id'],
+            'name' : user['name'],
+            'email' : user['email']
+        }
+    })
+    
+    return {'access_token':access_token}
+    
+@app.post('/signup')
+def signup(id:Annotated[str,Form()],
+           password:Annotated[str,Form()],
+            name:Annotated[str,Form()],
+            email:Annotated[str,Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return '200'
 
 @app.post('/items')
 #각각의 타입을 정해줌 (image : 파일 업로드 형식)
@@ -44,7 +80,7 @@ async def create_item(image:UploadFile,
     return '200'
 
 @app.get('/items')
-async def get_items():
+async def get_items(user=Depends(manager)):
     # 컬럼명도 같이 가져옴
     con.row_factory = sqlite3.Row
     cur = con.cursor()
@@ -62,17 +98,5 @@ async def get_image(item_id):
                             """).fetchone()[0]
     
     return Response(content=bytes.fromhex(image_bytes))
-    
-@app.post('/signup')
-def signup(id:Annotated[str,Form()],
-           password:Annotated[str,Form()],
-            name:Annotated[str,Form()],
-            email:Annotated[str,Form()]):
-    cur.execute(f"""
-                INSERT INTO users(id,name,email,password)
-                VALUES ('{id}','{name}','{email}','{password}')
-                """)
-    con.commit()
-    return '200'
     
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
